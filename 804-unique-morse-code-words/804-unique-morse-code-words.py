b1c03dccf7a3cda94ecd6bc512cfabcfb2286068
saivivek potnuru <70840432+saivivekpotnuru@users.noreply.github.com>
2022-08-17 19:14:34 +0530

    Time: 48 ms (67.93%), Space: 13.9 MB (24.27%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.py
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -1,22 +1,11 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        code=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        alpha = 'a'
-        alphabets=[]
-        for i in range(0, 26):
-            alphabets.append(alpha)
-            alpha = chr(ord(alpha) + 1)
-        s=dict(zip(alphabets,code))
-        res=[]
-        c=0
-        for i in words:
-            temp=""
-            for j in i:
-                temp+=s[j]
-            if temp in res:
-                continue
-            else:
-                c+=1
-                res.append(temp)
-        return c
+        table = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        seen = set()
+        for word in words:
+            transformation = ""
+            for c in word:
+                transformation += table[ord(c) - ord('a')]
+            seen.add(transformation)
+        return len(seen)
                 
